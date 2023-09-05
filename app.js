5800a31a451e824b5387e688517149d0f5777b6e
Billal-study <142863516+Billal-study@users.noreply.github.com>
2023-09-05 13:11:01 +0000

    next

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
+
+app.get('/billal',(req,res)=>{
+    res.render('index',{titleName:"Mr tech..."})
+})
 app.get('/',(req,res)=>{
     res.send("Billal Hossain home route")
 })
